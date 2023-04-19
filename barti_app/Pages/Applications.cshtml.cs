39f436e5f2dccbc85acdfd8c13f9ca2747acded3
bartek.pawlik <barti.pawlik@gmail.com>
2023-04-19 14:06:40 +0200

    Dodanie zakładki Applications, Timer oraz dodanie aplikacji stopera do API, która działa jak standarowy stoper

---
+++ barti_app/Pages/Applications.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class ApplicationsModel : PageModel
+    {
+        private readonly ILogger<ApplicationsModel> _logger;
+
+        public ApplicationsModel(ILogger<ApplicationsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ barti_app/Pages/Timer.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class TimerModel : PageModel
+    {
+        private readonly ILogger<TimerModel> _logger;
+
+        public TimerModel(ILogger<TimerModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
