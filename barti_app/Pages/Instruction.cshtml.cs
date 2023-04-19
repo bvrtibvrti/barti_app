b428a3a6e98848eef86628ee9124b505f02bc08d
bartek.pawlik <barti.pawlik@gmail.com>
2023-04-19 11:20:51 +0200

    Dodanie zakladki Instruction

---
+++ barti_app/Pages/Instruction.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class InstructionModel : PageModel
+    {
+        private readonly ILogger<InstructionModel> _logger;
+
+        public InstructionModel(ILogger<InstructionModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
