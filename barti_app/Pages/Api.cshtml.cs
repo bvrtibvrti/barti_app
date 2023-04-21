7348076a67dcb5544d99b1dec77dc0b8671ac861
bartek.pawlik <barti.pawlik@gmail.com>
2023-04-21 14:58:57 +0200

    Dodatnie zakladek API, Postman, RestApi oraz Knowledge

---
+++ barti_app/Pages/Api.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class ApiModel : PageModel
+    {
+        private readonly ILogger<ApiModel> _logger;
+
+        public ApiModel(ILogger<ApiModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ barti_app/Pages/RestApi.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class RestApiModel : PageModel
+    {
+        private readonly ILogger<RestApiModel> _logger;
+
+        public RestApiModel(ILogger<RestApiModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
