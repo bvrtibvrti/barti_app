f42766344e7fb0391a826d629357530dd8bcd1a7
bartek.pawlik <barti.pawlik@gmail.com>
2023-04-21 09:45:20 +0200

    Dodatnie linków w zakładce knowledge z odnosnikami do stron z wiedza na podany temat

---
+++ barti_app/Pages/Knowledge.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class KnowledgeModel : PageModel
+    {
+        private readonly ILogger<KnowledgeModel> _logger;
+
+        public KnowledgeModel(ILogger<KnowledgeModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
