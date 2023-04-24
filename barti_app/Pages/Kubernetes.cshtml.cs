01085174e8643fb6627b40a08216950863f99e4b
bartek.pawlik <barti.pawlik@gmail.com>
2023-04-24 10:39:22 +0200

    dodanie zakladki kubernetes

---
+++ barti_app/Pages/Kubernetes.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class KubernetesModel : PageModel
+    {
+        private readonly ILogger<KubernetesModel> _logger;
+
+        public KubernetesModel(ILogger<KubernetesModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
