c126e570e20766285be1beb377b8dcf15d3b17ba
bartek.pawlik <barti.pawlik@gmail.com>
2023-04-24 10:43:17 +0200

    dodanie zakladki terraform

---
+++ barti_app/Pages/Terraform.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class TerraformModel : PageModel
+    {
+        private readonly ILogger<PTerraformModel> _logger;
+
+        public TerraformModel(ILogger<TerraformModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
