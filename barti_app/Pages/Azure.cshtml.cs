dcc22c8be262a5def4b3789298fca8e0f6f8f726
bartek.pawlik <barti.pawlik@gmail.com>
2023-04-24 10:06:28 +0200

    aktualizacja kodu

---
+++ barti_app/Pages/Azure.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class AzureModel : PageModel
+    {
+        private readonly ILogger<AzureModel> _logger;
+
+        public AzureModel(ILogger<AzureModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
