1a2e664840462b468082434873579f093e618f50
bartek.pawlik <barti.pawlik@gmail.com>
2023-04-24 09:53:21 +0200

    Dodatnie zakladki powershell i git

---
+++ barti_app/Pages/Git.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class GitModel : PageModel
+    {
+        private readonly ILogger<GitModel> _logger;
+
+        public GitModel(ILogger<GitModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ barti_app/Pages/Powershell.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class PowershellModel : PageModel
+    {
+        private readonly ILogger<PowershellModel> _logger;
+
+        public PowershellModel(ILogger<PowershellModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
