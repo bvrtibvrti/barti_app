ceb6d14aac268ab3f56c15fa11772cced6d80009
bartek.pawlik <barti.pawlik@gmail.com>
2023-04-24 09:22:35 +0200

    Dodatnie zakladki Database

---
+++ barti_app/Pages/Database.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class DatabaseModel : PageModel
+    {
+        private readonly ILogger<DatabaseModel> _logger;
+
+        public DatabaseModel(ILogger<DatabaseModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
