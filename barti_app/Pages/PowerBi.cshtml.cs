6e19be8c211f9327c6e411505650059a88e86205
bartek.pawlik <barti.pawlik@gmail.com>
2023-04-24 11:07:53 +0200

    dodanie zakladki powerbi i zmiana w kodzie zakladki knowledge

---
+++ barti_app/Pages/PowerBi.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class PowerBiModel : PageModel
+    {
+        private readonly ILogger<PowerBiModel> _logger;
+
+        public PowerBiModel(ILogger<PowerBiModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
