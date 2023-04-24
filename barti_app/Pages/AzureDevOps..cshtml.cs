932f18c660e4e5ba238df14939d38c1829dc71d0
bartek.pawlik <barti.pawlik@gmail.com>
2023-04-24 10:10:04 +0200

    dodanie zakladki azure devops

---
+++ barti_app/Pages/AzureDevOps..cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BartiWebApp.Pages
+{
+    public class AzureDevOpsModel : PageModel
+    {
+        private readonly ILogger<AzureDevOpsModel> _logger;
+
+        public AzureDevOpsModel(ILogger<AzureDevOpsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
